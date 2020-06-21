6b837e4ff620e75c4832bb7fc09ef95d4192fda2
muratyldzxc <muratyldzxc@gmail.com>
2020-06-21 17:56:52 +0300

    Update Date.java

--- src/cse222/proje/Date.java
+++ src/cse222/proje/Date.java
@@ -1,6 +1,6 @@
 package cse222.proje;
 
-public class Date {
+public class Date implements Comparable<Date>{
 	/**
 	 * Holds the day
 	 */
@@ -18,6 +18,15 @@ public class Date {
 	 */
 	private double time;
 	
+	
+	
+	public Date(int day, int month, int year, double time) {
+		this.day = day;
+		this.month = month;
+		this.year = year;
+		this.time = time;
+	}
+
 	public int getDay() {
 		return day;
 	}
@@ -42,6 +51,45 @@ public class Date {
 	public void setTime(double time) {
 		this.time = time;
 	}
-	
-	
+
+	/**
+	 * Compare two date according to year, month, day, time in order
+	 * @param o will be compared
+	 * @return if given date is less return 1,  if given date is greater return -1, otherwise 0
+	 */
+	@Override
+	public int compareTo(Date o) {
+
+		if (this.year > o.year)
+			return 1;
+		else if (this.year < o.year)
+			return -1;
+
+		if (this.month > o.month)
+			return 1;
+		else if (this.month < o.month)
+			return -1;
+
+		if (this.day > o.day)
+			return 1;
+		else if (this.day < o.day)
+			return -1;
+
+		if (this.time > o.time)
+			return 1;
+		else if (this.time < o.time)
+			return -1;
+
+		return 0;
+	}
+
+	/**
+	 * Return Date Information as a String
+	 * @return Date Information as a String
+	 */
+	@Override
+	public String toString() {
+		return "Date: " + day + "." + month + "." + year + " Time: " + time;
+	}
+
 }
