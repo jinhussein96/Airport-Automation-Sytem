d923421413e1c33389552dbdad472a59960e5f07
muratyldzxc <muratyldzxc@gmail.com>
2020-06-21 17:57:41 +0300

    Update Hostess.java

--- src/cse222/proje/Hostess.java
+++ src/cse222/proje/Hostess.java
@@ -1,12 +1,12 @@
 package cse222.proje;
-
-import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.concurrent.ConcurrentSkipListSet;
 
 public class Hostess extends Employee{
     /**
      * Holds flights that hostess work on
      */
-    private ArrayList<Flight> flights;
+    private ConcurrentSkipListSet<Flight> flights;
 
     /**
      * Create a Hostess object
@@ -17,7 +17,7 @@ public class Hostess extends Employee{
      */
     public Hostess(String name, String surname, int ID, String password) {
         super(name, surname, ID, password);
-        this.flights = new ArrayList<Flight>();
+        this.flights = new ConcurrentSkipListSet<Flight>();
     }
 
     public Hostess() {
@@ -43,11 +43,27 @@ public class Hostess extends Employee{
     }
 
     /**
-     * Returns Hostess's flights as StringBuilder
-     * @return Hostess's flights as StringBuilder
+     * Returns Hostess's flights as String
+     * @return Hostess's flights as String
      */
-    public StringBuilder displayShift(){
-        return new StringBuilder();
+    public String getFlights(){
+    	String str = new String();
+        Iterator<Flight> iter = flights.iterator();
+
+        while (iter.hasNext()){
+            Flight temp = iter.next();
+            str += "\n Flight ID: " + temp.flightID + "\n Flight " + temp.flightDate + "\n Plane ID: " + temp.plane.planeID + "\n";
+        }
+        return "\n Hostes Flights \n" + str + "\n";
     }
 
+    /**
+     * Returns Information about Hostess like name, surname, ID
+     * @return Information about Hostess like name, surname, ID
+     */
+    public String toString() {
+    	
+    	return "Hostes ID: " + ID + "\n Hostes name: "+name + "\n Hostes surname: "+surname + "\n";
+    }
+    
 }
