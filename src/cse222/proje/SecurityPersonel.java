50df1439e3ba44ef63d7a9eacf7936ce0733c9d1
Omer Faruk <omerfsayar@gmail.com>
2020-06-23 14:11:02 +0300

    SecurityPersonel class is done.

--- src/cse222/proje/SecurityPersonel.java
+++ src/cse222/proje/SecurityPersonel.java
@@ -1,20 +1,9 @@
 package cse222.proje;
 
-public class SecurityPersonel extends Employee {
+public class SecurityPersonel extends Employee implements Comparable<Employee>{
 
-    /**
-     * Create a SecurityPersonel object
-     * @param name will be set
-     * @param surname will be set
-     * @param ID will be set
-     * @param password will be set
-     */
     public SecurityPersonel(String name, String surname, int ID, String password) {
         super(name, surname, ID, password);
-    }
-
-    public SecurityPersonel() {
-
     }
 
     /**
@@ -23,8 +12,19 @@ public class SecurityPersonel extends Employee {
      * @return if updated successfully returns true
      * @throws NullPointerException if one of given parameters is null
      */
-    public boolean updateSector(Sector sector){
-        return true;
+    public boolean updateSector(Sector sector)throws Exception{
+
+        if (sector.equals(null))
+            throw new NullPointerException("Sector can not be null!");
+
+        else {
+
+            if (sector.getSecurityState() == Sector.SecurityStates.SECURE)
+                return sector.updateSecurityState(Sector.SecurityStates.INSECURE);
+
+            else
+                return sector.updateSecurityState(Sector.SecurityStates.SECURE);
+        }
     }
 
     /**
@@ -36,4 +36,15 @@ public class SecurityPersonel extends Employee {
         System.out.printf("\n Sector %s security status : %s", sector.getSectorID(), sector.getSecurityState());
     }
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return "SecurityPersonel{" +
+                "ID=" + ID +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                '}';
+    }
 }
